1b0584a3b01834100b433162215e8cf24bd0ab02
lucas-masiero <lucasmasiero38@gmail.com>
2025-02-23 01:10:53 -0300

    :sparkles: feat(composite chords and middlewares): add support for managing composite chords and thread middlewares
    - Introduced Mount and Unmount methods to the Chord struct for adding and removing composite chords.
    - Added Use method to register thread wrappers (middlewares) in the Chord struct.
    - Implemented RegisterWrapped method to add wrapped threads to the Chord struct.
    - Updated documentation comments to reflect the new methods and their purposes.
    
    These changes enhance the flexibility of the Chord framework by allowing the registration of composite chords and the use of middlewares for thread management.

--- chord.go
+++ chord.go
@@ -36,31 +36,56 @@ type Thread func(Input, Output)
 
 // Chord holds a collection of threads, managed by a sync.Map.
 type Chord struct {
-	// multiplexer is a sync map that maps keys to threads
+	// threads is a sync map that maps keys to threads
 	// - key: string -> name thread
 	// - val: thread -> thread itself
-	multiplexer sync.Map
+	threads sync.Map
+	
+	// chords is a sync map that maps keys to composite type
+	// - key: string -> name chord
+	// - val: chord  -> chord itself
+	chords sync.Map
+
+	// middlewares is a slice of thread wrappers allowing to get wrapped in a pipeline pattern
+	// usable for both the threads and chords
+	// - wrapping is in FIFO order (first is the bigger wrapper)
+	middlewares []ThreadWrapper
 }
 
-// Register adds a thread to the multiplexer with the given key.
+// Register adds a thread to the threads with the given key.
 func (c *Chord) Register(key string, thread Thread) {
-	c.multiplexer.Store(key, thread)
+	c.threads.Store(key, thread)
 }
 
-// Unregister removes a thread from the multiplexer with the given key.
+// Unregister removes a thread from the threads with the given key.
 func (c *Chord) Unregister(key string, thread Thread) {
-	c.multiplexer.Delete(key)
+	c.threads.Delete(key)
+}
+
+// Mount adds a composite chord to the chords with the given key
+func (c *Chord) Mount(key string, chord *Chord) {
+	c.chords.Store(key, chord)
+}
+
+// Unmount removes a composite chord from the chords with the given key
+func (c *Chord) Unmount(key string) {
+	c.chords.Delete(key)
+}
+
+// Use allows to register a thread wrapper in the middlewares
+func (c *Chord) Use(tw ...ThreadWrapper) {
+	c.middlewares = append(c.middlewares, tw...)
 }
 
 // ThreadWrapper is a function type that wraps a Thread.
 type ThreadWrapper func(Thread) Thread
 
-// RegisterWrapped adds a wrapped thread to the multiplexer with the given key.
+// RegisterWrapped adds a wrapped thread to the threads with the given key.
 func (c *Chord) RegisterWrapped(key string, tw ...ThreadWrapper) {
 	var thread Thread
 	for i := range tw {
 		thread = tw[i](thread)
 	}
 	
-	c.multiplexer.Store(key, thread)
+	c.threads.Store(key, thread)
 }
